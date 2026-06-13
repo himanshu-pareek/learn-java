234d2581b43468440b52e0d742b0732e8f39c4c1
Himanshu Pareek <himanshupareekiit01@gmail.com>
2026-06-13 18:50:58 +0530

    Add some stream demos

---
+++ Streams/StreamsDemo.java
@@ -0,0 +1,161 @@
+void main() {
+    {
+        record City(String name, int population) {
+        }
+        record Country(String name, List<City> cities) {
+        }
+
+        City newYork = new City("New York", 8_258);
+        City losAngeles = new City("Los Angeles", 3_821);
+        Country usa = new Country("USA", List.of(newYork, losAngeles));
+
+        City london = new City("London", 8_866);
+        City manchester = new City("Manchester", 568);
+        Country uk = new Country("United Kingdom", List.of(london, manchester));
+
+        City paris = new City("Paris", 2_103);
+        City marseille = new City("Marseille", 877);
+        Country france = new Country("France", List.of(paris, marseille));
+
+        List<Country> countries = List.of(usa, uk, france);
+
+        var populationSummary = countries.stream()
+                .flatMap(country -> country.cities().stream())
+                .mapToInt(City::population)
+                .summaryStatistics();
+        IO.println("City population summary: " + populationSummary);
+    }
+
+    {
+        Function<String, Stream<Integer>> flatParser = s -> {
+            try {
+                return Stream.of(Integer.parseInt(s));
+            } catch (NumberFormatException _) {
+            }
+            return Stream.empty();
+        };
+
+        List<String> strings = List.of("1", " ", "2", "3 ", "", "3");
+
+        List<Integer> ints =
+                strings.stream()
+                        .flatMap(flatParser)
+                        .toList();
+        IO.println("ints = " + ints);
+    }
+
+    {
+        List<String> strings = List.of("1", " ", "2", "3 ", "", "3");
+
+        List<Integer> ints =
+                strings.stream()
+                        .<Integer>mapMulti(
+                                (string, consumer) -> {
+                                    try {
+                                        consumer.accept(Integer.parseInt(string));
+                                    } catch (NumberFormatException ignored) {
+                                        // Uncomment the following if you need to replace all the
+                                        // invalid numbers with -1
+                                        // consumer.accept(-1);
+                                    }
+                                })
+                        .toList();
+        IO.println("ints = " + ints);
+    }
+
+    {
+        List<Integer> ints = List.of(1, 4, 2, 1, 3, 3);
+
+        List<Integer> distinctNumbers =
+                ints.stream()
+                        .distinct()
+                        .toList();
+        IO.println("distinct ints: " + distinctNumbers);
+    }
+
+    {
+        var numbers = List.of("one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten");
+        List<String> naturallySorted = numbers.stream()
+                .sorted()
+                .toList();
+        IO.println("naturallySorted: " + naturallySorted);
+
+        List<String> sortedByLength = numbers.stream()
+                .sorted(Comparator.comparing(String::length))
+                .toList();
+        IO.println("Sorted by length: " + sortedByLength);
+    }
+    {
+        var ints = IntStream.iterate(0, i -> i + 1)
+                .map(i -> i /3)
+                .distinct()
+                .limit(5)
+                .toArray();
+        IO.println("ints = " + Arrays.toString(ints));
+    }
+    {
+        // The following code with run forever, since sorted will wait for all
+        // the elements (prerequisite of sorting)
+//        var ints = IntStream.iterate(0, i -> i + 1)
+//                .map(i -> i /3)
+//                .sorted()
+//                .limit(5)
+//                .toArray();
+//        IO.println("ints = " + Arrays.toString(ints));
+    }
+    {
+        List<Integer> ints = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9);
+
+        List<Integer> result =
+                ints.stream()
+                        .skip(2)
+                        .limit(5)
+                        .toList();
+        IO.println("result = " + result);
+    }
+    {
+        List<Integer> unDropped = Stream.of(1, 2, 3, 4, 3, 2, 1)
+                .dropWhile(x -> x < 4)
+                .toList();
+        IO.println("Un-dropped elements: " + unDropped);
+    }
+    {
+        List<Integer> takenElements = Stream.of(1, 2, 3, 4, 3, 2, 1)
+                .takeWhile(x -> x < 4)
+                .toList();
+        IO.println("Taken elements: " + takenElements);
+    }
+    {
+        List<Integer> list0 = List.of(1, 2, 3);
+        List<Integer> list1 = List.of(4, 5, 6);
+        List<Integer> list2 = List.of(7, 8, 9);
+
+        // 1st pattern: concat
+        List<Integer> concat =
+                Stream.concat(list0.stream(), list1.stream())
+                        .toList();
+
+        // 2nd pattern: flatMap
+        List<Integer> flatMap =
+                Stream.of(list0.stream(), list1.stream(), list2.stream())
+                        .flatMap(Function.identity())
+                        .toList();
+
+        IO.println("concat  = " + concat);
+        IO.println("flatMap = " + flatMap);
+    }
+    {
+        // WARNING: Don't use `peek` in production code and don't use it to do some side-effects
+        List<String> strings = List.of("one", "two", "three", "four");
+
+        List<String> result =
+                strings.stream()
+                        .peek(s -> IO.println("Starting with = " + s))
+                        .filter(s -> s.startsWith("t"))
+                        .peek(s -> IO.println("Filtered = " + s))
+                        .map(String::toUpperCase)
+                        .peek(s -> IO.println("Mapped = " + s))
+                        .toList();
+        IO.println("result = " + result);
+    }
+}
